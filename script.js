f5975f0d6082e94db42d55f65e78d9e8989aeb5c
RobertsKP2 <98320392+RobertsKP2@users.noreply.github.com>
2022-01-24 15:04:03 +0200

    Otrā stunda

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-const vardi=['Jana Krūmiņa','Jana Bēŗziņa','Dace'];
-const balvas=['Dators','Ledusskapis','Tālrunis','Auto'];
+const vardi=['Jana Krūmiņa','Jana Bēŗziņa','Dace']
+const balvas=['Dators','Ledusskapis','Tālrunis','Auto']
 const naudaKopa=100000;//kopeja summa
 let uzvaretajuSkaits=5;
 let rindas= document.querySelector('.rindas');
 rindas.innerHTML=' ';
-for(let i=0;i<5;i++){
+for(let i=0;i<uzvaretajuSkaits;i++){
 let rand= Math.random()*vardi.length;
 rand= Math.floor(rand);//noapaļo uz leju
 console.log(vardi[rand]);//izvada konsolē
 }
+rindas.innerHTML=
+<tr>
+    <td>i</td>
+    <td>uzvarētājs</td>
+</tr>
