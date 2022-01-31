a757a78c7ce31b10b8b5ce0f4b40f7fb5c04a5d5
RobertsKP2 <98320392+RobertsKP2@users.noreply.github.com>
2022-01-31 14:32:17 +0200

    2

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-const vardi=['Jana Krūmiņa','Jana Bēŗziņa','Dace']
-const balvas=['Dators','Ledusskapis','Tālrunis','Auto']
-const naudaKopa=100000;//kopeja summa
-let uzvaretajuSkaits=5;
-let rindas= document.querySelector('.rindas');
-rindas.innerHTML=' ';
-for(let i=0;i<uzvaretajuSkaits;i++){
-let rand= Math.random()*vardi.length;
-rand= Math.floor(rand);//noapaļo uz leju
-console.log(vardi[rand]);//izvada konsolē
-}
-rindas.innerHTML=
+const cilv = ["Aldon Hilter", "Josif Dzugashvilli", "Krišjānis Arturs Kariņš ", "Igors", "Ivans Timoshenko"];
+const balva = ["Dators ar Ryzen 9", "SAMSUNG A70", "McLaren P1", "Mercedes-Benz AMG GT Black Series", "GOOGLE"];
+const naudaKopa = 100000;//kopeja summa
+let uzvaretajuSkaits = 5;//uzvarētāju skaits
+let rand = Math.random() * 5;
+let rindas = document.querySelector('.rindas');
+rindas.innerHTML = ' ';
+
+for (let i = 0; i < uzvaretajuSkaits; i++) {
+    //let rand = Math.random()*vardi.length;
+    let rand = Math.random() * 5;
+    rand = Math.floor(rand);//noapaļo uz leju, math.floor - noapaļo uz leju
+
+    let uzvaretajs = vardi[rand];
+    rindas.innerHTML += `
 <tr>
-    <td>i</td>
-    <td>uzvarētājs</td>
-</tr>
+<td>${i+1}</td>
+<td>${uzvaretajs}</td>
+</tr>`
+}
